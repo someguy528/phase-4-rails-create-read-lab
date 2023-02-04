f2c705234fbc90ff694087ad1ec7351584a3a115
Brandon Castro <castroyou999@gmail.com>
2023-02-04 18:19:04 -0500

    commit tree c600d3be3fea9a3774c82b130edec633835cd30b

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,19 @@
+class PlantsController < ApplicationController
+
+    def index
+        plants = Plant.all 
+        render json: plants
+    end
+    def show
+        plant = Plant.find(params[:id])
+        if plant
+            render json: plant
+        else 
+            render json: {error: "Plant not found"}, status: :not_found
+        end
+    end
+    def create
+        plant = Plant.create(params.permit(:name, :image, :price))
+        render json: plant, status: :created
+    end
+end
